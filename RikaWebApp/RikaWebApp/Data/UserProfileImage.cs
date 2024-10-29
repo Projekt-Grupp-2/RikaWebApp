7bc1007d12bfdff42cfe5a5d6b5cc5092c2e5e6c
Carl <carllindqvist93@gmail.com>
2024-10-29 13:46:58 +0100

    Klar med sätta upp identity

---
+++ RikaWebApp/RikaWebApp/Data/UserProfileImage.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RikaWebApp.Data;
+
+public class UserProfileImage
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    public string ProfileImage { get; set; } = null!;
+
+}
