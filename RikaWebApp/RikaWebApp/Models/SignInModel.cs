ae81b739f9cd9f1d555d1fbf8a0248ecfe080efe
Farangis Ölund <bless.faro@gmail.com>
2024-10-30 14:46:45 +0100

    SignInm Model added and validation is done

---
+++ RikaWebApp/RikaWebApp/Models/SignInModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RikaWebApp.Models;
+
+public class SignInModel
+{
+    [Display(Name = "Email", Prompt = "Email")]
+    [DataType(DataType.EmailAddress)]
+    [Required(ErrorMessage = "Email address is required")]
+    public string Email { get; set; } = null!;
+
+    [Display(Name = "Password", Prompt = "Password")]
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; } = null!;
+}
